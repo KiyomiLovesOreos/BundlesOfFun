4722292622131f9f07d8b6c1cbf05e9cec7579c3
kierkat10 <kiernan.simpson@gmail.com>
2026-07-05 22:24:35 -0700

    commit farming 2

--- items/coupons/dark_alley.lua
+++ items/coupons/dark_alley.lua
@@ -2,12 +2,12 @@ BundlesOfFun.Voucher {
     key = "dark_alley",
     name = "Dark Alley",
     bundle = "coupons",
-    config = { spectral_rate = 1 },
+    config = { extra = { spectral_rate = 1 } },
     pos = { x = 0, y = 0 },
     atlas = "voucher",
     redeem = function(self, card, area)
-        if G.GAME.spectral_rate < self.config.spectral_rate then
-            G.GAME.spectral_rate = self.config.spectral_rate
+        if G.GAME.spectral_rate < self.config.extra.spectral_rate then
+            G.GAME.spectral_rate = self.config.extra.spectral_rate
         end
     end
 }
